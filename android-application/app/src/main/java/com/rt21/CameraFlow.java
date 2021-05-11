2dc67d005b9c8f3c8332fcf1b32e9872ba6106b6
Marko Hiršel <80032554+markoHirsel@users.noreply.github.com>
2021-05-11 18:13:06 +0200

    delete unnecessary code

--- android-application/app/src/main/java/com/rt21/CameraFlow.java
+++ android-application/app/src/main/java/com/rt21/CameraFlow.java
@@ -1,22 +1,5 @@
 package com.rt21;
 
-import android.util.Size;
-
-import androidx.camera.core.AspectRatio;
-import androidx.camera.core.ImageAnalysis;
-import androidx.camera.core.ImageCapture;
-
 public class CameraFlow {
-    ImageCapture imageCapture =
-            new ImageCapture.Builder()
-                    .setFlashMode(ImageCapture.FLASH_MODE_OFF)
-            .setTargetAspectRatio(AspectRatio.RATIO_16_9)
-        .build();
-
-    ImageAnalysis imageAnalysis =
-            new ImageAnalysis.Builder()
-                    .setTargetResolution(new Size(640, 480))
-                    .build();
-
 
 }
