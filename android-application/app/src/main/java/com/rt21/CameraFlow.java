7db792f43205f00d0c7c771f616d09bd08c15ad5
Marko Hiršel <80032554+markoHirsel@users.noreply.github.com>
2021-05-12 18:05:43 +0200

    Added takeImage method which needs implementation

--- android-application/app/src/main/java/com/rt21/CameraFlow.java
+++ android-application/app/src/main/java/com/rt21/CameraFlow.java
@@ -1,12 +1,21 @@
 package com.rt21;
 
+import android.content.Context;
 import android.view.TextureView;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class CameraFlow {
 
     static void getCameraFlowPreview(TextureView textureView){
 
 
+    }
+
+    static boolean takeImage(File imageFile, Context context, ImageView imageView, TextureView textureView) {
+        //TODO: needs implementation
+        return false;
     }
 
 }
