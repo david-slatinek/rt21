30ba2f9826e5ce1e140b0190d50a3bccf33d6be7
Marcel Iskrač <40259973+iskraM@users.noreply.github.com>
2021-05-14 21:45:39 +0200

    Prepared functions for API login and register requests
    - needs further testing and implementation for receiving data

--- android-application/app/src/main/java/com/rt21/LoginActivity.java
+++ android-application/app/src/main/java/com/rt21/LoginActivity.java
@@ -4,14 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+import com.koushikdutta.ion.Response;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.sql.Time;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import timber.log.Timber;
 import timber.log.Timber;
@@ -34,6 +59,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etEmail;
     private EditText etPassword;
+    private EditText etAge;
     private Button btnLogin_Register;
     private CheckBox chkStayLoggedIn;
 
@@ -49,6 +75,7 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etEmail = findViewById(R.id.etEmail);
         etPassword = findViewById(R.id.etPassword);
+        etAge = findViewById(R.id.etAge);
         btnLogin_Register = findViewById(R.id.login_registerBtn);
         chkStayLoggedIn = findViewById(R.id.chk_StayLoggedIn);
 
@@ -61,25 +88,26 @@ public class LoginActivity extends AppCompatActivity {
         if (activityMode == FORM_MODE_LOGIN) {
             txtTitle.setText(getString(R.string.login_page_title));
             etName.setVisibility(View.GONE);
-            etEmail.setVisibility(View.GONE);
+            etUsername.setVisibility(View.GONE);
+            etAge.setVisibility(View.GONE);
             btnLogin_Register.setText(getString(R.string.login_button));
             btnLogin_Register.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (!etUsername.getText().toString().equals("") && !etPassword.getText().toString().equals("")) {
-                        //TODO - check if user exists
-                        //if (login.success) {
-                        if (chkStayLoggedIn.isChecked()) {
-                            SharedPreferences sprefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-                            SharedPreferences.Editor editor = sprefs.edit();
-                            editor.putBoolean("remember_user", true);
-                            editor.apply();
+                    if (!etEmail.getText().toString().equals("") && !etPassword.getText().toString().equals("")) {
+                        boolean success = onLogin();
+                        if (success) {
+                            if (chkStayLoggedIn.isChecked()) {
+                                SharedPreferences sprefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+                                SharedPreferences.Editor editor = sprefs.edit();
+                                editor.putBoolean("remember_user", true);
+                                editor.apply();
+                            }
+                            Timber.i("Login pressed");
+                            returnBack(1);
+                        } else {
+                            CommonMethods.displayToastShort("User doesn't exist!", getApplicationContext());
                         }
-                        Timber.i("Login pressed");
-                        returnBack(1);
-                        //} else {
-                        //    CommonMethods.displayToastShort(getApplicationContext(), "User doesn't exist!");
-                        //}
                     } else {
                         CommonMethods.displayToastLong("No login data provided!", getApplicationContext());
                     }
@@ -87,20 +115,20 @@ public class LoginActivity extends AppCompatActivity {
             });
         } else if (activityMode == FORM_MODE_REGISTER) {
             txtTitle.setText(getString(R.string.register_page_title));
-            chkStayLoggedIn.setChecked(false);
+            chkStayLoggedIn.setVisibility(View.GONE);
             btnLogin_Register.setText(getString(R.string.register_button));
             btnLogin_Register.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     if (!etName.getText().toString().equals("") && !etUsername.getText().toString().equals("") &&
                             !etEmail.getText().toString().equals("") && !etPassword.getText().toString().equals("")) {
-                        //TODO - check if user exists and if data is compatible for registration
-                        //if (registration.success) {
-                        Timber.i("Register pressed");
-                        returnBack(0);
-                        //} else {
-                        //    CommonMethods.displayToastShort(getApplicationContext(), "Registration failed!");
-                        //}
+                        boolean success = onRegistration();
+                        if (success) {
+                            Timber.i("Register pressed");
+                            returnBack(0);
+                        } else {
+                            CommonMethods.displayToastShort("Registration failed!", getApplicationContext());
+                        }
                     } else {
                         CommonMethods.displayToastShort("No registration data provided!", getApplicationContext());
                     }
@@ -117,5 +145,71 @@ public class LoginActivity extends AppCompatActivity {
             setResult(RESULT_OK, i);
         }
         finish();
+    }
+
+    //TODO - i works but Callback doesn't start
+    private boolean onLogin() {
+        /*Ion.with(getBaseContext())
+                .load("POST","https://rt21-api.herokuapp.com/api/user/login")
+                .setHeader(app.getKeyName(), app.getApiKey())
+                .setBodyParameter("email", etEmail.getText().toString())
+                .setBodyParameter("password", etPassword.getText().toString())
+                .asJsonObject()
+                .withResponse()
+                .setCallback(new FutureCallback<Response<JsonObject>>() {
+                    @Override
+                    public void onCompleted(Exception e, Response<JsonObject> result) {
+                        if (result != null) {
+                            if (result.getHeaders().code() == 200) {
+                                Timber.i("onCompleted success");
+                                Timber.i(result.toString());
+                            } else {
+                                Timber.i("Error!");
+                            }
+                        }
+                    }
+                });*/
+
+        /**
+         * Testing: request to get data of a testUser from API
+         * Ion sends GET request to api with id in url and header that contains api key name and api key value
+         * as a result it gets JsonObject
+         */
+        try {
+            JsonObject json = Ion.with(this)
+                    .load("https://rt21-api.herokuapp.com/api/user/609e9bc42327d6e860ff7113")
+                    .setHeader(app.getKeyName(), app.getApiKey())
+                    .asJsonObject()
+                    .get();
+
+            Timber.i(json.toString());
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+    //TODO - i works but Callback doesn't start
+    private boolean onRegistration() {
+        Ion.with(getBaseContext())
+                .load("POST","https://rt21-api.herokuapp.com/api/user/login")
+                .setHeader(app.getKeyName(), app.getApiKey())
+                .setMultipartParameter("name", etName.getText().toString().split("\\s+")[0])
+                .setMultipartParameter("last_name", etName.getText().toString().split("\\s+")[1])
+                .setMultipartParameter("age", etAge.getText().toString())
+                .setMultipartParameter("age", "20")
+                .setMultipartParameter("nickname", etUsername.getText().toString())
+                .setMultipartParameter("email", etEmail.getText().toString())
+                .setMultipartParameter("password", etPassword.getText().toString())
+                .asJsonObject()
+                .setCallback(new FutureCallback<JsonObject>() {
+                    @Override
+                   public void onCompleted(Exception e, JsonObject result) {
+                       Timber.i(result.toString());
+                   }
+                });
+
+        return true;
     }
 }
