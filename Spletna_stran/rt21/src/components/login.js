ad062e3ccde270c624da2bc9aaed185f9d739824
Marcel Iskrač <40259973+iskraM@users.noreply.github.com>
2021-05-26 20:10:48 +0200

    Added login and register page

--- Spletna_stran/rt21/src/components/login.js
+++ Spletna_stran/rt21/src/components/login.js
@@ -2,8 +2,25 @@ import React from 'react';
 
 const Login = (props) => {
     return (
-        <div>
-            <h1>Login {props.user}</h1>
+        <div className="card w-75 m-auto mt-5">
+            <div className="card-header text-center">
+                <h3>Login</h3>
+            </div>
+            <div className="card-body">
+                <form>
+                    <div className="input-group mb-3">
+                        <span className="input-group-addon"><i className="glyphicon glyphicon-user"></i></span>
+                        <input type="text" className="form-control" name="username" placeholder="Username"/>
+                    </div>
+                    <div className="input-group mb-3">
+                        <span className="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                        <input type="password" className="form-control" name="password" placeholder="Password"/>
+                    </div>
+                    <div className="text-center">
+                        <input type="submit" className="btn btn-primary w-50" style={{fontSize: 17}} value="Login"/>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
