833fac6a6d1be2ce4fd9d46ef93dab7dd36d1cc4
David Slatinek <david.slatinek5@gmail.com>
2021-06-01 11:44:26 +0200

    Added age input in register form

--- website/src/components/register.js
+++ website/src/components/register.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 const Register = () => {
     const [fullname, setFullname] = useState('');
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [age, setAge] = useState('');
 
     const [errorMessage, setErrorMessage] = useState('');
 
@@ -17,7 +18,7 @@ const Register = () => {
         const formData = new FormData();
         formData.append("name", fullnameSplit[0]);
         formData.append("last_name", fullnameSplit[1]);
-        formData.append("age", 21);
+        formData.append("age", age);
         formData.append("nickname", username);
         formData.append("email", email);
         formData.append("password", password);
@@ -29,27 +30,30 @@ const Register = () => {
             },
             body: formData
         })
-        .then(response => {
-            if (!response.ok) {
-                return response.json().then(response => {throw new Error(response.error)})
-            } 
-            return response.json();
-        })
-        .then(data => {
-            console.log("success");
+            .then(response => {
+                if (!response.ok) {
+                    return response.json().then(response => {
+                        throw new Error(response.error)
+                    })
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log("success");
 
-            setFullname("");
-            setUsername("");
-            setEmail("");
-            setPassword("");
+                setFullname("");
+                setUsername("");
+                setEmail("");
+                setPassword("");
+                setAge("");
 
-            console.log(data);
+                console.log(data);
 
-            window.location = '/login';
-        }) 
-        .catch((error) => {
-            setErrorMessage('' + error);
-        });
+                window.location = '/login';
+            })
+            .catch((error) => {
+                setErrorMessage('' + error);
+            });
     }
 
 
@@ -59,32 +63,57 @@ const Register = () => {
                 <h3>Register</h3>
             </div>
             <div className="card-body">
-                <form>
+                <form onSubmit={onRegister}>
                     <div className="input-group mb-3">
-                        <div class="input-group-text"></div>
-                        <input type="text" className="form-control" name="fullname" placeholder="Full name" required="" value={fullname} onChange={(e)=>{setFullname(e.target.value)}}/>
+                        <div className="input-group-text"><i className="fas fa-user-edit"/></div>
+                        <input type="text" className="form-control" name="fullname" placeholder="Name and lastname" required
+                               value={fullname} onChange={(e) => {
+                            setFullname(e.target.value)
+                        }}/>
                     </div>
+
                     <div className="input-group mb-3">
-                        <div class="input-group-text">📧</div>
-                        <input type="text" className="form-control" name="email" placeholder="Email" required="" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+                        <div className="input-group-text"><i className="fas fa-envelope"/></div>
+                        <input type="email" className="form-control" name="email" placeholder="Email" required
+                               value={email} onChange={(e) => {
+                            setEmail(e.target.value)
+                        }}/>
                     </div>
+
                     <div className="input-group mb-3">
-                        <div class="input-group-text">👤</div>
-                        <input type="text" className="form-control" name="username" placeholder="Username" required="" value={username} onChange={(e)=>{setUsername(e.target.value)}}/>
+                        <div className="input-group-text"><i className="fas fa-user-alt"/></div>
+                        <input type="text" className="form-control" name="username" placeholder="Username" required
+                               value={username} onChange={(e) => {
+                            setUsername(e.target.value)
+                        }}/>
                     </div>
+
                     <div className="input-group mb-3">
-                        <div className="input-group-text">🔒</div>
-                        <input type="password" className="form-control" name="password" placeholder="Password" required="" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+                        <div className="input-group-text"><i className="far fa-calendar"/></div>
+                        <input type="number" className="form-control" name="age" placeholder="Age"
+                               required value={age} onChange={(e) => {
+                            setAge(e.target.value)
+                        }}/>
                     </div>
+
+
+                    <div className="input-group mb-3">
+                        <div className="input-group-text"><i className="fas fa-key"/></div>
+                        <input type="password" className="form-control" name="password" placeholder="Password"
+                               required value={password} onChange={(e) => {
+                            setPassword(e.target.value)
+                        }}/>
+                    </div>
+
                     <div className="text-center">
-                        <input type="button" className="btn btn-primary w-50" style={{fontSize: 17}} value="Register" onClick={onRegister}/>
+                        <input type="submit" className="btn btn-dark w-50" style={{fontSize: 17}} value="Register"/>
                     </div>
                 </form>
             </div>
-            { errorMessage !== '' &&
-                <div style={{background: '#f8d7da'}} className="card-footer border-danger">
-                    <span style={{color: '#721c24'}}>{errorMessage}</span> 
-                </div>
+            {errorMessage !== '' &&
+            <div style={{background: '#f8d7da'}} className="card-footer border-danger">
+                <span style={{color: '#721c24'}}>{errorMessage}</span>
+            </div>
             }
         </div>
     )
