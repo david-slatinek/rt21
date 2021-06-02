3687762129d7fb86e661e5ab719c4de8dc36cc6b
David Slatinek <david.slatinek5@gmail.com>
2021-06-02 21:26:11 +0200

    Added Dockerfile and script for uploading website to the server

---
+++ website/heroku.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+heroku container:login
+#heroku create rt21-website --region eu
+docker image build -t rt21-web .
+heroku container:push web --app rt21-website
+heroku container:release web --app rt21-website
