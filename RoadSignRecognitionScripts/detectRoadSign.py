c00ba3b72c53d3295f3c7c4f6dddc211a101e4b3
Marcel Iskrač <40259973+iskraM@users.noreply.github.com>
2021-06-06 17:23:26 +0200

    Finished traffic sign detection
    - script takes path to image and returns what type of sign it finds on it

--- RoadSignRecognitionScripts/detectRoadSign.py
+++ RoadSignRecognitionScripts/detectRoadSign.py
@@ -1,17 +1,64 @@
+import os
 import cv2
-import numpy as np
+import sys
+import getopt
 
-from tensorflow import keras
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'    # INFO, ERROR AND WARNINGS are not prined
+import tensorflow
+import numpy as np
+import pandas as pd
+
 from skimage import io
 from keras import models
+from tensorflow import keras
 
+def main(argv):
+    image = ''
+    
+    # parse argv values
+    try:
+        opts, args = getopt.getopt(argv, "hi:", ["inputfile="])
+    except getopt.GetoptError:
+        print('python detectRoadSign.py -i <inputfile>')
+        sys.exit(2)
 
-model = models.load_model("classifier_model")
-class_names = ["Speed limit (50km/h)", "General caution", "Stop"]
-#img = cv2.imread("testing/50.jpg")
-img = cv2.imread("StopSignDataset/6.jpg")
+    if opts != []:
+        for opt, arg in opts:
+            if opt == '-h': # print help for running script
+                print('python detectRoadSign.py -i <inputfile>')
+                sys.exit()
+            elif opt in ('-i', '--inputfile'):  # parse given path for image
+                image = arg
+            else:   # error: incorect flags given
+                print("Run: \"python detectRoadSign.py -h\" for help.") 
+                sys.exit()
+    else:   # no arguments give
+        print("No arguments were given...")
+        print("Run: \"python detectRoadSign.py -h\" for help.")
+        sys.exit()
+        # image = "StopSignDataset/16.jpg"
 
-img = cv2.resize(img, (32, 32), interpolation = cv2.INTER_LINEAR)
-prediction = model.predict(np.array([img]))
-index = np.argmax(prediction)
-print(class_names[index])
+    # load model that contains data for predicting roadsings
+    model = models.load_model("classifier_model")
+    
+    data= pd.read_csv("labels.csv")
+    class_names = []
+    # store roadsing names
+    for _, row in data.iterrows():
+        class_names.append(row["Name"])
+ 
+    # prepare image for predicting the roadsings
+    img = cv2.imread(image)
+    img = np.asarray(img)
+    img = cv2.resize(img, (32, 32), interpolation = cv2.INTER_LINEAR)
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img = img/255
+    img = img.reshape(1, 32, 32, 1)
+
+    prediction = model.predict(img)
+    index = np.argmax(prediction)
+    print(class_names[index])
+
+if __name__ == "__main__":
+    # python detectRoadSign.py -i testing/stop.png
+    main(sys.argv[1:])
--- RoadSignRecognitionScripts/modelTraining.py
+++ RoadSignRecognitionScripts/modelTraining.py
@@ -1,46 +1,126 @@
+import os
 import cv2
-import glob
-from cv2 import data
+import random
+import tensorflow
 import numpy as np
-from numpy.lib.polynomial import RankWarning
-from tensorflow.python.keras import layers
-from keras import layers, models
+import pandas as pd
+import matplotlib.pyplot as plt
 
-
-data_indexes = [2, 14, 18]
-class_names = ["Speed limit (50km/h)", "Stop", "General caution"]
-
+from keras.layers import Dense
+from keras.optimizers import Adam
+from keras.models import Sequential
+from keras.layers import Dropout, Flatten
+from keras.utils.np_utils import to_categorical
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+ 
+### Parameters ###
+path = "myData"             # folder with all the class folders
+labelFile = 'labels.csv'    # file with all names of classes
+batch_size_val=50           # how many to process together
+epochs_val=10
+imageDimesions = (32,32,3)
+testRatio = 0.2
+validationRatio = 0.2
+ 
+### Importing the Images ###
+count = 0
 images = []
-labels = []
-
-print("Importing data...")
-print("Number of roadsings to learn: ", len(data_indexes))
-for i in range(0, len(data_indexes)):
-    path = "myData/" + str(data_indexes[i]) + "/*"
-    for filename in glob.glob(path):
-        img = cv2.imread(filename)
-        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-        img = cv2.resize(img, (32, 32), interpolation = cv2.INTER_LINEAR)
-        images.append(img)
-        labels.append(i)
-
-    print(data_indexes[i], end=" ")
-print()
-print("Import done")
-
+classNo = []
+myList = os.listdir(path)
+print("Total Classes Detected:",len(myList))
+noOfClasses=len(myList)
+print("Importing Classes.....")
+for x in range (0,len(myList)):
+    myPicList = os.listdir(path+"/"+str(count))
+    for y in myPicList:
+        curImg = cv2.imread(path+"/"+str(count)+"/"+y)
+        images.append(curImg)
+        classNo.append(count)
+    print(count, end =" ")
+    count +=1
+print(" ")
 images = np.array(images)
-labels = np.array(labels)
+classNo = np.array(classNo)
+ 
+### Split Data ###
+X_train, X_test, y_train, y_test = train_test_split(images, classNo, test_size=testRatio)
+X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=validationRatio)
+ 
+# X_train = ARRAY OF IMAGES TO TRAIN
+# y_train = CORRESPONDING CLASS ID
+ 
+### CHECK IF NUMBER OF IMAGES MATCHES THE NUMBER OF LABELS FOR EACH DATA SET ###
+print("Data Shapes")
+print("Train",end = "");print(X_train.shape,y_train.shape)
+print("Validation",end = "");print(X_validation.shape,y_validation.shape)
+print("Test",end = "");print(X_test.shape,y_test.shape)
+assert(X_train.shape[0]==y_train.shape[0]), "The number of images in not equal to the number of lables in training set"
+assert(X_validation.shape[0]==y_validation.shape[0]), "The number of images in not equal to the number of lables in validation set"
+assert(X_test.shape[0]==y_test.shape[0]), "The number of images in not equal to the number of lables in test set"
+assert(X_train.shape[1:]==(imageDimesions))," The dimesions of the Training images are wrong "
+assert(X_validation.shape[1:]==(imageDimesions))," The dimesionas of the Validation images are wrong "
+assert(X_test.shape[1:]==(imageDimesions))," The dimesionas of the Test images are wrong" 
+ 
+### PREPROCESSING THE IMAGES ###
+def preprocessing(img):
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)  # CONVERT TO GRAYSCALE
+    img = cv2.equalizeHist(img)                 # STANDARDIZE THE LIGHTING IN AN IMAGE
+    img = img/255                               # TO NORMALIZE VALUES BETWEEN 0 AND 1 INSTEAD OF 0 TO 255
+    return img
+ 
+X_train=np.array(list(map(preprocessing,X_train)))  # TO ITERATE AND PREPROCESS ALL IMAGES
+X_validation=np.array(list(map(preprocessing,X_validation)))
+X_test=np.array(list(map(preprocessing,X_test)))
+ 
+### ADD A DEPTH OF 1 ###
+X_train=X_train.reshape(X_train.shape[0],X_train.shape[1],X_train.shape[2],1)
+X_validation=X_validation.reshape(X_validation.shape[0],X_validation.shape[1],X_validation.shape[2],1)
+X_test=X_test.reshape(X_test.shape[0],X_test.shape[1],X_test.shape[2],1)
+ 
+### AUGMENTATAION OF IMAGES: TO MAKE IT MORE GENERIC ###
+dataGen= ImageDataGenerator(width_shift_range=0.1,
+                            height_shift_range=0.1,
+                            zoom_range=0.2,
+                            shear_range=0.1,
+                            rotation_range=10)
+dataGen.fit(X_train)
+batches= dataGen.flow(X_train,y_train,batch_size=20)  # REQUESTING DATA GENRATOR TO GENERATE IMAGES
+X_batch,y_batch = next(batches) 
 
-model = models.Sequential()
-model.add(layers.Conv2D(32,(3,3),activation="relu",input_shape=(32,32,3)))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(64,(3,3),activation="relu"))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(64,(3,3),activation="relu"))
-model.add(layers.Flatten())
-model.add(layers.Dense(64,activation="relu"))
-model.add(layers.Dense(10,activation="softmax"))
+y_train = to_categorical(y_train,noOfClasses)
+y_validation = to_categorical(y_validation,noOfClasses)
+y_test = to_categorical(y_test,noOfClasses)
+ 
+### CONVOLUTION NEURAL NETWORK MODEL ###
+def myModel():
+    no_Of_Filters=60
+    size_of_Filter=(5,5) # KERNEL THAT MOVES AROUND THE IMAGE TO GET THE FEATURES
+    size_of_Filter2=(3,3)
+    size_of_pool=(2,2)  # SCALE DOWN ALL FEATURE MAP TO GERNALIZE MORE, TO REDUCE OVERFITTING
+    no_Of_Nodes = 500   # NUM OF NODES IN HIDDEN LAYERS
 
-model.compile(optimizer="adam",loss="sparse_categorical_crossentropy",metrics=["accuracy"])
-model.fit(images,labels,epochs=10,verbose=1)
+    model= Sequential()
+    model.add((Conv2D(no_Of_Filters,size_of_Filter,input_shape=(imageDimesions[0],imageDimesions[1],1),activation='relu')))  # ADDING MORE CONVOLUTION LAYERS = LESS FEATURES BUT CAN CAUSE ACCURACY TO INCREASE
+    model.add((Conv2D(no_Of_Filters, size_of_Filter, activation='relu')))
+    model.add(MaxPooling2D(pool_size=size_of_pool))
+ 
+    model.add((Conv2D(no_Of_Filters//2, size_of_Filter2,activation='relu')))
+    model.add((Conv2D(no_Of_Filters // 2, size_of_Filter2, activation='relu')))
+    model.add(MaxPooling2D(pool_size=size_of_pool))
+    model.add(Dropout(0.5))
+ 
+    model.add(Flatten())
+    model.add(Dense(no_Of_Nodes,activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(noOfClasses,activation='softmax'))
+
+    model.compile(Adam(lr=0.001),loss='categorical_crossentropy',metrics=['accuracy'])
+    return model
+ 
+### TRAIN ###
+model = myModel()
+print(model.summary())
+history=model.fit_generator(dataGen.flow(X_train,y_train,batch_size=batch_size_val), epochs=epochs_val,validation_data=(X_validation,y_validation),shuffle=1)
 model.save("classifier_model")
