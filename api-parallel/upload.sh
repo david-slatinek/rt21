5b693af97ccebb541eafd77885b39dde1dc0d31c
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2022-01-09 13:56:28 +0100

    Remove api-parrallel/

--- api-parallel/upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-heroku container:login
-#heroku create rt21-api --region eu
-docker image build -t rt21-api .
-heroku container:push web --app rt21-api
-heroku container:release web --app rt21-api
