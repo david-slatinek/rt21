287261f547199606451d15844246c8f1e09b8080
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2022-01-18 15:22:27 +0100

    Rename folder RGA to ar-app, move files in documents/

--- algorithm/demo.sh
+++ algorithm/demo.sh
@@ -4,6 +4,6 @@ KEY=""
 URL=""
 
 curl "$URL" -H "X-API-Key: $KEY" -o compressed.bin
-python3 main.py -d compressed.bin >> numbers.txt
+python3 main.py -d compressed.bin > numbers.txt
 echo
 cat numbers.txt
